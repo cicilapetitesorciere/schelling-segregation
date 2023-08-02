6a7d633082a10c438b6463d63e5ca3d04becbe3c
Ciaran Neely <cgtneely@uwaterloo.ca>
2023-08-01 21:36:04 -0400

    Fixed documentation

--- board.py
+++ board.py
@@ -16,90 +16,68 @@ Point = Tuple[int, int]
 X: Final = 0
 Y: Final = 1
 
-# Generates the points which draw a square entred at `centre` such that each of the
-# four walls is offset r units away from the centre
-#
-# For instance, r=1 and centre=(10,-1) produces the following points:
-#
-# (11, 0), (10, 0),
-# (9, -2), (10, -2),
-# (9, 0), (9, -1),
-# (11, -2), (11, -1)
-#
-# or visually:
-#
-# x x x
-# x c x
-# x x x
-#
-# and r=2 with centre=(0,0) produces:
-# (2, 2), (1, 2), (0, 2), (-1, 2),
-# (-2, -2), (-1, -2), (0, -2), (1, -2),
-# (-2, 2), (-2, 1), (-2, 0), (-2, -1),
-# (2, -2), (2, -1), (2, 0), (2, 1)
-#
-# x x x x x
-# x       x
-# x   c   x
-# x       x
-# x x x x x
-#
-# This is achieved by generating each of the four walls of the square, each of
-# which is given one corner. For instance, in the r=2 example, we can number
-# each wall as from 1 to 4 to get the following and add arrows to show the
-# direction in which the wall is generated, producing the following:
-#
-#   ------>
-#   2 2 2 2 4 |
-# ^ 3       4 |
-# | 3   c   4 |
-# | 3       4 v
-# | 3 1 1 1 1
-#     <------
-#
-# Notice that this ordering corresponds to the order of the points listed for
-# r=2. It is also analagous to the points listed for r=1.
-#
-# The algorithm is easiest to explain if we start by assuming centre=(0,0)
-#
-# Each wall's positition relative to the centre can be generated using two numbers
-#  -- one for the axis and a second for the direction in which the wall travels.
-# For instance in the above r=2 example, wall 1 travels along the x-axis in the
-# negative direction, wall 2 travels along the x-axis in the positive direction,
-# wall 3 travels along the y-axis in the negative direction, and wall 4 travels
-# along the y-axis in the positive direction.
-#
-# The four walls run respectively along the following lines
-# 1: y = +r (travels toward negative x)
-# 2: y = -r (travels toward positve x)
-# 3: x = -r (travels toward negative y)
-# 4: x = +r (travels toward positive y)
-#
-# It's easy to exhaustively check that the RHS generalizes to
-# r*direction*(2*axis-1) where axis=0 indicates the x-axis, and axis=1
-# represents the y-axis
-#
-# Corners of any square can be defined by the set of points (a,b) such that
-# |a|=|b|. Thus a corner on a square whose walls are each r units away from the
-# origin has the additional property |a|=|b|=r. Hence a wall (not including
-# corners) travelling parallel to the x-axis is characterized by
-# y=-(r-1),-(r-2),...,r-1 if the direction is positive, or
-# y=r-1,r-2,...,-(r-1) if the direction is negative (notice that this second set
-# of points is simply the first set with each element multiplied by -1, making
-# it convienient to represent the direction of travel as either +1 for positive
-# and -1 for negative). To include a corner at the start of the path, we add the
-# point y=r*(-direction)=-r*direction where direction=+1 indicates travel toward
-# positive y, and direction=-1 indicates travel toward negative y. This allows
-# us to generalize the y-values to
-#
-#        y=-r*direction,-(r-1)*direction,...,(r-1)*direction
-#
-# Similar properties apply to walls travelling along the y-axis.
-#
-# Generalizing the function to other centres is then just a case of adding
-# pair-wisethe elements of the new centre to the zero-centred output points
-#
+
 def square(r: int = 1, centre: Point = (0, 0)) -> Iterator[Point]:
+    """
+    Generates the points which draw a square entred at `centre` such that each of the four walls is offset r units away from the centre
+    
+    For instance, r=1 and centre=(10,-1) produces the following points:
+    
+    (11, 0), (10, 0),
+    (9, -2), (10, -2),
+    (9, 0), (9, -1),
+    (11, -2), (11, -1)
+    
+    or visually:
+    
+    x x x
+    x c x
+    x x x
+    
+    and r=2 with centre=(0,0) produces:
+    (2, 2), (1, 2), (0, 2), (-1, 2),
+    (-2, -2), (-1, -2), (0, -2), (1, -2),
+    (-2, 2), (-2, 1), (-2, 0), (-2, -1),
+    (2, -2), (2, -1), (2, 0), (2, 1)
+    
+    x x x x x
+    x       x
+    x   c   x
+    x       x
+    x x x x x
+    
+    This is achieved by generating each of the four walls of the square, each of which is given one corner. For instance, in the r=2 example, we can number each wall as from 1 to 4 to get the following and add arrows to show the direction in which the wall is generated, producing the following:
+    
+      ------>
+      2 2 2 2 4 |
+    ^ 3       4 |
+    | 3   c   4 |
+    | 3       4 v
+    | 3 1 1 1 1
+        <------
+    
+    Notice that this ordering corresponds to the order of the points listed for r=2. It is also analagous to the points listed for r=1.
+    
+    The algorithm is easiest to explain if we start by assuming centre=(0,0)
+    
+    Each wall's positition relative to the centre can be generated using two numbers -- one for the axis and a second for the direction in which the wall travels. For instance in the above r=2 example, wall 1 travels along the x-axis in the negative direction, wall 2 travels along the x-axis in the positive direction, wall 3 travels along the y-axis in the negative direction, and wall 4 travels along the y-axis in the positive direction.
+    
+    The four walls run respectively along the following lines
+    1: y = +r (travels toward negative x)
+    2: y = -r (travels toward positve x)
+    3: x = -r (travels toward negative y)
+    4: x = +r (travels toward positive y)
+    
+    It's easy to exhaustively check that the RHS generalizes to r*direction*(2*axis-1) where axis=0 indicates the x-axis, and axis=1 represents the y-axis
+    
+    Corners of any square can be defined by the set of points (a,b) such that |a|=|b|. Thus a corner on a square whose walls are each r units away from the origin has the additional property |a|=|b|=r. Hence a wall (not including corners) travelling parallel to the x-axis is characterized by y=-(r-1),-(r-2),...,r-1 if the direction is positive, or y=r-1,r-2,...,-(r-1) if the direction is negative (notice that this second set of points is simply the first set with each element multiplied by -1, making it convienient to represent the direction of travel as either +1 for positive and -1 for negative). To include a corner at the start of the path, we add the point y=r*(-direction)=-r*direction where direction=+1 indicates travel toward positive y, and direction=-1 indicates travel toward negative y. This allows us to generalize the y-values to
+    
+           y=-r*direction,-(r-1)*direction,...,(r-1)*direction
+    
+    Similar properties apply to walls travelling along the y-axis.
+    
+    Generalizing the function to other centres is then just a case of adding pair-wisethe elements of the new centre to the zero-centred output points
+    """
     assert r >= 1
     # Chooses wall
     for axis in [X, Y]:
@@ -211,8 +189,10 @@ class Board:
             for _ in range(self.populations[species]):
                 self[vacant_squares.pop(0)] = species
 
-    # Returns True iff `xy` is a valid point on the board
     def includes_point(self, xy: Point) -> bool:
+        """
+        Returns True iff `xy` is a valid point on the board
+        """
         return (0 <= xy[X] < self.shape[X]) and (0 <= xy[Y] < self.shape[Y])
 
     def __getitem__(self, xy: Point) -> int:
@@ -239,22 +219,25 @@ class Board:
             ret += "\n"
         return ret
 
-    # Moves a the agent located at `old` to the point `new`
-    def move(self, old: Point, new: Point) -> None:
+    def move(self, start: Point, end: Point) -> None:
+        """
+        Moves the agent located at `start` to `end`
+        """
         if (
-            self.includes_point(old)
-            and self.includes_point(new)
-            and self[old] != -1
-            and self[new] == -1
+            self.includes_point(start)
+            and self.includes_point(end)
+            and self[start] != -1
+            and self[end] == -1
         ):
-            self[new] = self[old]
-            self[old] = -1
+            self[end] = self[start]
+            self[start] = -1
         else:
             raise IllegalMoveError
 
-    # Returns the proportion of neighbours which are conspecific to the agent at
-    # `xy`
     def conspecificity(self, xy: Point) -> float:
+        """
+        Returns the proportion of neighbours which are conspecific to the agent located at `xy`
+        """
         number_of_neighbours: int = 0
         number_of_similar_neighbours: int = 0
         species_of_target = self[xy]
@@ -271,12 +254,18 @@ class Board:
         else:
             return number_of_similar_neighbours / number_of_neighbours
 
-    # Returns True if the unit at position xy is disatisfied and False otherwise
+    
     def is_satisfied(self, xy: Point) -> bool:
+        """
+        Returns True if the unit at position `xy` is disatisfied
+        """
         return self.conspecificity(xy) >= self.thresholds[self[xy]]
 
-    # Returns the total number of agents on the board who are satisfied
+    
     def total_satisfied(self) -> int:
+        """
+        Returns the total number of agents on the board who are satisfied
+        """
         ret: int = 0
         for x in range(self.shape[X]):
             for y in range(self.shape[Y]):
@@ -284,30 +273,22 @@ class Board:
                     ret += 1
         return ret
 
-    # Runs one full round of the simulation
+    
     def update(self):
+        """
+        Runs one full round of the simulation
+        """
 
-        # Finds a suitable point for the agent at `xy` to move to and moves the
-        # agent to that location and produces that point as a return-value.
-        #
-        # Schelling allows for a variety of algorithms to achieve this. In this
-        # this particular case we use the following algorithm:
-        #
-        # The agent first chooses a search space. This search space will include
-        # at least the spaces immediately surrounding the agent (including
-        # corners). The agent then has the option to expand their search space
-        # outwards by one unit. If they do choose to expand the search space, they
-        # are given the option to expand it again. This outward expansion may
-        # theoretically continue forever. At each iteratetion, the probability
-        # that the agent chooses to expand the search space is 1-`proximity_bias`.
-        #
-        # Once the search space is chosen, the agent will randomly choose a point
-        # from it to try to move to. If the piece is unable to succesfully make
-        # the move (i.e. it would move the piece off the board or if another agent
-        # is already occupying the chosen space) the agent will discard their
-        # first choice and choose another. If their are no open spots in the
-        # agent's search space, it will stay put.
         def find_and_move_to_new_spot(xy: Point) -> Point:
+            """
+            Finds a suitable point for the agent at `xy` to move to and moves the agent to that location and produces that point as a return-value.
+            
+            Schelling allows for a variety of algorithms to achieve this. In this this particular case we use the following algorithm:
+            
+            The agent first chooses a search space. This search space will include at least the spaces immediately surrounding the agent (including corners). The agent then has the option to expand their search space outwards by one unit. If they do choose to expand the search space, they are given the option to expand it again. This outward expansion may theoretically continue forever. At each iteratetion, the probability that the agent chooses to expand the search space is `1-proximity_bias`.
+            
+            Once the search space is chosen, the agent will randomly choose a point from it to try to move to. If the piece is unable to succesfully make the move (i.e. it would move the piece off the board or if another agent is already occupying the chosen space) the agent will discard their first choice and choose another. If their are no open spots in the agent's search space, it will stay put.
+            """
             r = 1
             searchspace: List[Point] = list(square(r=r, centre=xy))
             while random() >= self.proximity_bias[self[xy]]:
